db83dabb3e20bb8eeec6b577fb3fce82cb5c4365
Hemant Singh <HemantSingh@fico.com>
2014-08-15 18:01:58 -0700

    Initial commit

---
+++ js/gp.js
@@ -0,0 +1,14 @@
+var gpListItems = [ 
+	["Durga / दुर्गा", "durga", "<a class='btn btn-primary' href='/durga/chalisa.html'>Chalisa</a> &nbsp;<a class='btn btn-primary' href='/durga/aarti.html'>Aarti</a>"],
+	["Ganesh / गणेश", "ganesh", "<a class='btn btn-primary' href='/ganesh/chalisa.html'>Chalisa</a>&nbsp;<a class='btn btn-primary' href='/ganesh/aarti.html'>Aarti</a>"],
+	["Hanuman / हनुमान", "hanuman", "<a class='btn btn-primary' href='/hanuman/chalisa.html'>Chalisa</a>&nbsp;<a class='btn btn-primary' href='/hanuman/aarti.html'>Aarti</a>"],
+	["Krishna / कृष्णा", "krishna", "<a class='btn btn-primary' href='/krishna/chalisa.html'>Chalisa</a> &nbsp;<a class='btn btn-primary' href='/krishna/aarti.html'>Aarti</a>"],
+	["Laxmi / लक्ष्मी", "laxmi", "<a class='btn btn-primary' href='/laxmi/chalisa.html'>Chalisa</a> &nbsp;<a class='btn btn-primary' href='/laxmi/aarti.html'>Aarti</a>"],
+	["MahaKali / काली", "kali", "<a class='btn btn-primary' href='/kali/chalisa.html'>Chalisa</a> &nbsp;<a class='btn btn-primary' href='/kali/aarti.html'>Aarti</a>"],
+	["Ram / राम", "ram", "<a class='btn btn-primary' href='/ram/chalisa.html'>Chalisa</a> &nbsp;<a class='btn btn-primary' href='/ram/aarti.html'>Aarti</a>"],
+	["Sai Baba / साई बाबा", "sai", "<a class='btn btn-primary' href='/sai/chalisa.html'>Chalisa</a> &nbsp;<a class='btn btn-primary' href='/sai/aarti.html'>Aarti</a>"],
+	["Saraswati / सरस्वती", "saraswati", "<a class='btn btn-primary' href='/saraswati/chalisa.html'>Chalisa</a> &nbsp;<a class='btn btn-primary' href='/saraswati/aarti.html'>Aarti</a>"],
+	["Shani / शनि", "shani", "<a class='btn btn-primary' href='/shani/chalisa.html'>Chalisa</a> &nbsp;<a class='btn btn-primary' href='/shani/aarti.html'>Aarti</a>"],
+	["Shiv / शिव", "shiv", "<a class='btn btn-primary' href='/shiv/chalisa.html'>Chalisa</a> &nbsp;<a class='btn btn-primary' href='/shiv/aarti.html'>Aarti</a>"],
+	["Vishnu / विष्णु", "vishnu", "<a class='btn btn-primary' href='/vishnu/chalisa.html'>Chalisa</a> &nbsp;<a class='btn btn-primary' href='/vishnu/aarti.html'>Aarti</a>"]
+];
